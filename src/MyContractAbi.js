15db9afa26a8bccec7af5e3f87a476848cf2b398
Anil Kumar <anilkr9810@gmail.com>
2025-08-31 23:08:00 +0530

    Fixed import errors in App.js and MyContractAbi.js

--- src/MyContractAbi.js
+++ src/MyContractAbi.js
@@ -1,3 +1,2 @@
-const abi = process.env.REACT_APP_CONTRACT_ABI;
-const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS;
-export { abi, contractAddress };
+export const abi = process.env.REACT_APP_CONTRACT_ABI;
+export const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS;
